0a65d371b580fc1f6a4b3b370b30015d89996315
Svetoslav Ivanov <svetoslav.ivanov@cloudrisk.co.uk>
2019-07-01 11:31:45 +0300

    LIMQUET

---
+++ LIMQUET/root/reg.php
@@ -0,0 +1,139 @@
+<?php
+
+include("form.html");
+// Receiving variables
+@$pfw_ip= $_SERVER['REMOTE_ADDR'];
+@$Title = addslashes($_POST['Title']);
+@$first_name = addslashes($_POST['first_name']);
+@$sur_name = addslashes($_POST['sur_name']);
+@$gender = addslashes($_POST['gender']);
+@$email = addslashes($_POST['email']);
+@$address = addslashes($_POST['address']);
+@$title_of_the_presentation = addslashes($_POST['title_of_the_presentation']);
+@$abstract = addslashes($_POST['abstract']);
+@$comments = addslashes($_POST['comments']);
+
+// Validation
+if (strlen($first_name) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid first name</font></p>");
+}
+
+if (strlen($sur_name) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid sur name</font></p>");
+}
+
+if (! ereg('[A-Za-z0-9_-]+\@[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+', $email))
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid email</font></p>");
+}
+
+if (strlen($email) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid email</font></p>");
+}
+
+if (strlen($address) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid address</font></p>");
+}
+
+//Sending Email to form owner
+$pfw_header = "From: $sur_name\n"
+  . "Reply-To: $sur_name\n";
+$pfw_subject = "CAMEL registration";
+$pfw_email_to = "camel15@quantum-bg.org";
+$pfw_message = "Visitor's IP: $pfw_ip\n"
+. "Title: $Title\n"
+. "first_name: $first_name\n"
+. "sur_name: $sur_name\n"
+. "gender: $gender\n"
+. "email: $email\n"
+. "address: $address\n"
+. "title_of_the_presentation: $title_of_the_presentation\n"
+. "abstract: $abstract\n"
+. "comments: $comments\n";
+@mail($pfw_email_to, $pfw_subject ,$pfw_message ,$pfw_header ) ;
+
+
+//Sending Email to svetljo@gmail.com
+$pfw_header = "From: $sur_name\n"
+  . "Reply-To: $sur_name\n";
+$pfw_subject = "CAMEL registration";
+$pfw_email_to = "svetljo@gmail.com";
+//
+$pfw_message = "\\\\title{" . strtoupper($title_of_the_presentation) . "}\n"
+. "% $title_of_the_presentation\n\n"
+. "\underline{" . $first_name[0] . ". " . $sur_name . "} \index{" . $sur_name . " " . $first_name[0] . ".} \n"
+. "%" . $first_name . " " . $sur_name . "\n\n"
+. "{\\\\normalsize{\\\\vspace{-4mm}\n"
+. "$address\n\n"
+. "$nationality\n\n"
+. "\email " . "$email" . "}}\n\n"
+. "$abstract\n\n"
+. "\\\\vspace{\baselineskip}";
+@mail($pfw_email_to, $pfw_subject ,$pfw_message ,$pfw_header ) ;
+
+
+//Sending Email to vitanov@phys.uni-sofia.bg
+$pfw_header = "From: $sur_name\n"
+  . "Reply-To: $sur_name\n";
+$pfw_subject = "CAMEL registration";
+$pfw_email_to = "vitanov@phys.uni-sofia.bg";
+//
+$pfw_message = "\\\\title{" . strtoupper($title_of_the_presentation) . "}\n"
+. "% $title_of_the_presentation\n\n"
+. "\underline{" . $first_name[0] . ". " . $sur_name . "} \index{" . $sur_name . " " . $first_name[0] . ".} \n"
+. "%" . $first_name . " " . $sur_name . "\n\n"
+. "{\\\\normalsize{\\\\vspace{-4mm}\n"
+. "$address\n\n"
+. "$nationality\n\n"
+. "\email " . "$email" . "}}\n\n"
+. "$abstract\n\n"
+. "\\\\vspace{\baselineskip}";
+@mail($pfw_email_to, $pfw_subject ,$pfw_message ,$pfw_header ) ;
+
+
+//Sending Email to genko.genov@gmail.com
+$pfw_header = "From: $sur_name\n"
+  . "Reply-To: $sur_name\n";
+$pfw_subject = "CAMEL transport information";
+$pfw_email_to = "genko.genov@gmail.com";
+//
+$pfw_message = 
+"Title: $Title\n"
+. "first_name: $first_name\n"
+. "sur_name: $sur_name\n"
+. "email: $email\n"
+. "comments: $comments\n";
+@mail($pfw_email_to, $pfw_subject ,$pfw_message ,$pfw_header ) ;
+
+
+//Sending auto respond Email to visitor
+
+$pfw2_header = "From: CAMEL";
+$pfw2_subject = "CAMEL registration successful";
+$pfw2_email_to = "$email\n ";
+$pfw2_message = "Thank you for your abstract submission for CAMEL workshop. The following form was sent to us : "
+."
+"
+."
+"
+. "Title: $Title\n"
+. "first_name: $first_name\n"
+. "sur_name: $sur_name\n"
+. "gender: $gender\n"
+. "email: $email\n"
+. "address: $address\n"
+. "title_of_the_presentation: $title_of_the_presentation\n"
+. "abstract: $abstract\n"
+. "comments: $comments\n"
+. "Please do not reply to this email, it is automatic respond. "
+. "If you want to contact us for further information, please write us at the following address camel15@quantum-bg.org ";
+@mail(@$email, $pfw2_subject ,$pfw2_message ,$pfw2_header ) ;
+
+
+ echo("<p align='center'><font face='Arial' size='3' color='#FF0000'>Your registration form was successfully sent</font></p>");
+?>
+
---
+++ LIMQUET/root/tr.php
@@ -0,0 +1,136 @@
+<?php
+// include("trform.html");
+// Receiving variables
+@$pfw_ip= $_SERVER['REMOTE_ADDR'];
+@$name = addslashes($_POST['name']);
+@$email = addslashes($_POST['email']);
+@$hotel = addslashes($_POST['hotel']);
+@$flight_arrival = addslashes($_POST['flight_arrival']);
+@$arriving_from = addslashes($_POST['arriving_from']);
+@$arriving_date_ = addslashes($_POST['arriving_date_']);
+@$arriving_time_ = addslashes($_POST['arriving_time_']);
+
+@$flight_departure = addslashes($_POST['flight_departure']);
+@$departing_to_ = addslashes($_POST['departing_to_']);
+@$departing_date_ = addslashes($_POST['departing_date_']);
+@$departing_time_ = addslashes($_POST['departing_time_']);
+
+// Validation
+if (strlen($name) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid name</font></p>");
+}
+
+if (! ereg('[A-Za-z0-9_-]+\@[A-Za-z0-9_-]+\.[A-Za-z0-9_-]+', $email))
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid email</font></p>");
+}
+
+if (strlen($email) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid email</font></p>");
+}
+
+if (strlen($flight_arrival) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid arrival flight number</font></p>");
+}
+
+if (strlen($flight_departure) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid departure flight number</font></p>");
+}
+
+if (strlen($arriving_from) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid arriving from</font></p>");
+}
+
+if (strlen($departing_to_) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid departing to</font></p>");
+}
+
+if (strlen($arriving_date_) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid arriving date</font></p>");
+}
+
+if (strlen($departing_date_) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid departing date</font></p>");
+}
+
+if (strlen($arriving_time_) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid arriving time</font></p>");
+}
+
+if (strlen($departing_time_) == 0 )
+{
+die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter a valid departing time</font></p>");
+}
+
+//Sending Email to form owner
+$pfw_header = "From: $name\n"
+  . "Reply-To: $name\n";
+$pfw_subject = "CAMEL15 transportation form";
+$pfw_email_to = "camel15@quantum-bg.org";
+$pfw_message = "Visitor's IP: $pfw_ip\n"
+."
+"
+. "name: $name\n"
+. "email: $email\n"
+. "accomodated at Hotel: $hotel\n"
+."
+"
+. "flight_arrival: $flight_arrival\n"
+. "arriving_from: $arriving_from\n"
+. "arriving_date_: $arriving_date_\n"
+. "arriving_time_: $arriving_time_\n"
+."
+"
+. "flight_departure: $flight_departure\n"
+. "departing_to_: $departing_to_\n"
+. "departing_date_: $departing_date_\n"
+. "departing_time_: $departing_time_\n"
+."
+"
+. "If you want to contact us for further information, please write us at the following address camel15@quantum-bg.org ";
+@mail($pfw_email_to, $pfw_subject ,$pfw_message ,$pfw_header ) ;
+
+//Sending auto respond Email to visitor
+
+
+$pfw2_header = "From: CAMEL15";
+$pfw2_subject = "CAMEL15 transportation form sent successful";
+$pfw2_email_to = "$email\n ";
+$pfw2_message = "The following transportation form was sent to us : "
+."
+"
+."
+"
+. "name: $name\n"
+. "email: $email\n"
+. "accomodated at Hotel: $hotel\n"
+."
+"
+. "flight_arrival: $flight_arrival\n"
+. "arriving_from: $arriving_from\n"
+. "arriving_date_: $arriving_date_\n"
+. "arriving_time_: $arriving_time_\n"
+."
+"
+. "flight_departure: $flight_departure\n"
+. "departing_to_: $departing_to_\n"
+. "departing_date_: $departing_date_\n"
+. "departing_time_: $departing_time_\n"
+."
+"
+. "Please do not reply to this email, it is automatic response. "
+. "If you want to contact us for further information, please write us at the following address camel15@quantum-bg.org ";
+@mail(@$email, $pfw2_subject ,$pfw2_message ,$pfw2_header ) ;
+
+
+ echo("<p align='center'><font face='Arial' size='3' color='#FF0000'>Your transportaiton form was successfully sent</font></p>");
+?>
