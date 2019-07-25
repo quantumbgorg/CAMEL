7f1d5e1ac9990a8bb24dba0dc8cfc8052af8ce5e
Svetoslav Ivanov <svetoslav.ivanov@cloudrisk.co.uk>
2019-07-25 10:38:00 +0300

    accommodation.html and reg.php

--- LIMQUET/root/reg.php
+++ LIMQUET/root/reg.php
@@ -41,12 +41,12 @@ die("<p align='center'><font face='Arial' size='3' color='#FF0000'>Please enter 
 
 
 //Sending Email to svetljo@gmail.com
-$pfw_header = "From: $sur_name\n"
+$header = "From: $sur_name\n"
   . "Reply-To: $sur_name\n";
-$pfw_subject = "LIMQUET School registration";
-$to = 'svetljo@gmail.com';
+$subject = "LIMQUET School registration";
+$to = "svetljo@gmail.com";
 //
-$pfw_message = "\\\\title{" . strtoupper($title_of_the_presentation) . "}\n"
+$message = "\\\\title{" . strtoupper($title_of_the_presentation) . "}\n"
 . "% $title_of_the_presentation\n\n"
 . "\underline{" . $first_name[0] . ". " . $sur_name . "} \index{" . $sur_name . " " . $first_name[0] . ".} \n"
 . "%" . $first_name . " " . $sur_name . "\n\n"
@@ -56,15 +56,15 @@ $pfw_message = "\\\\title{" . strtoupper($title_of_the_presentation) . "}\n"
 . "\email " . "$email" . "}}\n\n"
 . "$abstract\n\n"
 . "\\\\vspace{\baselineskip}";
-@mail($to, $pfw_subject ,$pfw_message ,$pfw_header ) ;
+@mail($to, $subject, $message, $header);
 
 
 //Sending auto respond Email to visitor and genko.genov@gmail.com, vitanov@phys.uni-sofia.bg, limquet@quantum-bg.org
 
-$pfw2_header = "From: LIMQUET_School";
-$pfw2_subject = "LIMQUET School registration successful";
-$pfw2_email_to = "$email, genko.genov@gmail.com, vitanov@phys.uni-sofia.bg, limquet@quantum-bg.org";
-$pfw2_message = "Thank you for your abstract submission for LIMQUET school. The following form was sent to us: "
+$header = "From: LIMQUET_School";
+$subject = "LIMQUET School registration successful";
+$to = "$email, genko.genov@gmail.com, vitanov@phys.uni-sofia.bg, limquet@quantum-bg.org";
+$message = "Thank you for your abstract submission for LIMQUET school. The following form was sent to us: "
 ."
 "
 ."
@@ -80,9 +80,8 @@ $pfw2_message = "Thank you for your abstract submission for LIMQUET school. The 
 . "comments: $comments\n"
 . "Please do not reply to this email, it is automatic respond. "
 . "If you want to contact us for further information, please write us at the following address limquet@quantum-bg.org ";
-@mail(@$email, $pfw2_subject ,$pfw2_message ,$pfw2_header ) ;
+@mail($to, $subject, $message, $header);
 
 
  echo("<p align='center'><font face='Arial' size='3' color='#FF0000'>Your registration form was successfully sent. The confirmation message may be in your spam folder.</font></p>");
 ?>
-
